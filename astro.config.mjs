ee2cfaa5ad5572c8cb42a86e416e66aafe36633d
Jeremy Pittard <jeremy@jpittard.net>
2024-09-30 22:48:30 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  experimental: {
+    env: {
+      schema: {
+        SWAUS_KEY: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false,
+        }),
+        SWAUS_URL: envField.string({
+          context: "server",
+          access: "public",
+          optional: false,
+        }),
+        BZ_URL: envField.string({
+          context: "server",
+          access: "public",
+          optional: false,
+        }),
+        PUBLIC_BMAC_URL: envField.string({
+          context: "client",
+          access: "public",
+          optional: true,
+        }),
+      },
+    },
+  },
+});
